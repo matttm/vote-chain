3b02c9fc73c447bed840eee33d14b99cbbb66ae0
matttm <matttmaloney@gmail.com>
2024-04-13 18:41:57 -0400

    removing p2p delegate

--- internal/messenger/messenger.go
+++ internal/messenger/messenger.go
@@ -1,17 +1,28 @@
 package messenger
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 	"context"
 	"encoding/json"
-
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	"github.com/libp2p/go-libp2p/core/peer"
 
 	"vote-chain/pkg/models"
 	"vote-chain/pkg/topics"
 	"vote-chain/internal/utilities"
 )
 
+
+// DiscoveryInterval is how often we re-publish our mDNS records.
+const DiscoveryInterval = time.Hour
+
+// DiscoveryServiceTag is used in our mDNS advertisements to discover other chat peers.
+const DiscoveryServiceTag = "pubsub-chat-example"
 // ChatRoomBufSize is the number of incoming messages to buffer for each topic.
 const ChatRoomBufSize = 128
 
@@ -33,12 +44,37 @@ type Messenger struct {
 	nick     string
 }
 
+// discoveryNotifee gets notified when we find a new peer via mDNS discovery
+type discoveryNotifee struct {
+	h host.Host
+}
+
 // type stateMessage struct {
 // 	Payload models.State
 // 	SenderID string
 // }
 func CreateMessenger() *Messenger {
 	m := new(Messenger)
+	m.ctx = context.Background()
+
+	protocolId := "/ip4/0.0.0.0/tcp/0"
+	// create a new libp2p Host that listens on a random TCP port
+	h, err := libp2p.New(libp2p.ListenAddrStrings(protocolId))
+	if err != nil {
+		panic(err)
+	}
+
+	// create a new PubSub service using the GossipSub router
+	floodsub, err := pubsub.NewFloodSub(m.ctx, h)
+	if err != nil {
+		panic(err)
+	}
+	m.ps = floodsub
+
+	// setup local mDNS discovery
+	if err := setupDiscovery(h); err != nil {
+		panic(err)
+	}
 	return m
 }
 
@@ -61,7 +97,7 @@ func (msngr *Messenger) ListPeers() []peer.ID {
 }
 
 // readLoop pulls messages from the pubsub topic and pushes them onto the Messages channel.
-func (msngr *Messenger) ReadLoop() {
+func (msngr *Messenger) readLoop() {
 	for {
 		msg, err := msngr.sub.Next(msngr.ctx)
 		if err != nil {
@@ -83,16 +119,36 @@ func (msngr *Messenger) ReadLoop() {
 }
 
 
-func (msngr *Messenger) ListenToVoteChain(ctx context.Context, ps *pubsub.PubSub, selfID peer.ID) {
+func (msngr *Messenger) ListenToVoteChain() {
 	// join the pubsub topic
-	topic := utilities.CreateTopicHandle(ps, topics.CHAIN)
+	topic := utilities.CreateTopicHandle(m.ps, topics.CHAIN)
 
 	// and subscribe to it
 	sub, err := topic.Subscribe()
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
+	msngr.sub = sub
 	// start reading messages from the subscription in a loop
 	// ToDo:  put this kon its own thread
-	go msngr.ReadLoop()
+	go msngr.readLoop()
+}
+
+// HandlePeerFound connects to peers discovered via mDNS. Once they're connected,
+// the PubSub system will automatically start interacting with them if they also
+// support PubSub.
+func (n *discoveryNotifee) HandlePeerFound(pi peer.AddrInfo) {
+	fmt.Printf("discovered new peer %s\n", pi.ID)
+	err := n.h.Connect(context.Background(), pi)
+	if err != nil {
+		fmt.Printf("error connecting to peer %s: %s\n", pi.ID, err)
+	}
+}
+
+// setupDiscovery creates an mDNS discovery service and attaches it to the libp2p Host.
+// This lets us automatically discover peers on the same LAN and connect to them.
+func setupDiscovery(h host.Host) error {
+	// setup mDNS discovery to find local peers
+	s := mdns.NewMdnsService(h, DiscoveryServiceTag, &discoveryNotifee{h: h})
+	return s.Start()
 }
